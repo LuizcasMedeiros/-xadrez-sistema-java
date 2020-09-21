97aa68ae7b0949eb8a522bf5a46b6b6e81ada874
Luiz Filipe <luizfelipedecastro@gmail.com>
2020-09-21 20:12:25 -0300

    Classe de inicio: Posicao

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,9 +1,14 @@
 package aplicacao;
 
+import jogotabueiro.Posicao;
+
+
+
 public class Programa {
 
 	public static void main(String[] args) {
-		
+		Posicao posicao = new Posicao(3,5);
+		System.out.println(posicao);
 
 	}
 
